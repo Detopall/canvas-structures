78123bcb5249da632d31e889e0d958c7b523050b
denis.topallaj <denis.topallaj@student.howest.be>
2022-09-06 22:28:13 +0200

    Added color and new canvas, fixed header position (flexed).

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,32 @@ function init(){
 	showVortex();
 	showSpiral();
 	showCube();
+	showSunshine();
+}
+
+function showSunshine(){ //could've done more, but RAM depleted
+	const canvas = document.querySelector("#sunshine");
+	const ctx = canvas.getContext('2d');
+
+	for (let i = 0; i < canvas.width; i += WIDTH) {
+		const cell = new Cell(i, canvas.height, 0, 0);
+		cell.draw(ctx, "orange");
+
+		for (let j = 0; j < canvas.width; j += WIDTH) {
+			const cell = new Cell(canvas.height, j, 0, 0);
+			cell.draw(ctx, "red");
+
+			for (let k = 0; k < canvas.width; k += WIDTH) {
+				const cell = new Cell(canvas.height, 0, k, canvas.height);
+				cell.draw(ctx, "green");
+
+				for (let l = 0; l < canvas.width; l += WIDTH) {
+					const cell = new Cell(canvas.height, l, 0, canvas.height);
+					cell.draw(ctx, "blue");
+				}
+			}
+		}
+	}
 }
 
 function showCube(){
@@ -18,32 +44,32 @@ function showCube(){
 	const ctx = canvas.getContext('2d');
 
 	const a = new Cell(40, 80, 40, 180);
-	a.draw(ctx);
+	a.draw(ctx, "orange");
 	const b = new Cell(40, 180, 140, 180);
-	b.draw(ctx);
+	b.draw(ctx, "orange");
 	const c = new Cell(140, 180, 140, 80);
-	c.draw(ctx);
+	c.draw(ctx, "orange");
 	const d = new Cell(40, 80, 140, 80);
-	d.draw(ctx);
+	d.draw(ctx, "orange");
 
 	const e = new Cell(40, 80, 80, 60);
-	e.draw(ctx);
+	e.draw(ctx, "red");
 	const f = new Cell(80, 60, 170, 60);
-	f.draw(ctx);
+	f.draw(ctx, "red");
 	const g = new Cell(140, 80, 170, 60);
-	g.draw(ctx);
+	g.draw(ctx, "red");
 	const h = new Cell(140, 180, 170, 160);
-	h.draw(ctx);
+	h.draw(ctx, "red");
 	const i = new Cell(170, 60, 170, 160);
-	i.draw(ctx);
+	i.draw(ctx, "red");
 
 	ctx.setLineDash([8, 5]); //set the next lines to dashed lines
 	const j = new Cell(40, 180, 80, 160);
-	j.draw(ctx);
+	j.draw(ctx, "blue");
 	const k = new Cell(80, 160, 170, 160);
-	k.draw(ctx);
+	k.draw(ctx, "blue");
 	const l = new Cell(80, 160, 80, 60);
-	l.draw(ctx);
+	l.draw(ctx, "blue");
 }
 
 function showSpiral(){
@@ -51,11 +77,11 @@ function showSpiral(){
 	const ctx = canvas.getContext('2d');
 	for (let i = 0; i < canvas.width; i+=WIDTH) {
 		const cell = new Cell(i, 0, canvas.height, i);
-		cell.draw(ctx);
+		cell.draw(ctx, "blue");
 
 		for (let j = 0; j < canvas.width; j+=WIDTH) {
 			const cell = new Cell(0, j, j, canvas.height);
-			cell.draw(ctx);
+			cell.draw(ctx, "red");
 		}
 	}
 }
@@ -66,7 +92,7 @@ function showVortex(){
 	for (let i = 0; i < canvas.width; i+=WIDTH) {
 		for (let j = 0; j < canvas.width; j+=WIDTH) {
 			const cell = new Cell(canvas.height-WIDTH/2, i+WIDTH/2, WIDTH/2, j+WIDTH/2);
-			cell.draw(ctx);
+			cell.draw(ctx, "yellow");
 		}
 	}
 }
@@ -76,10 +102,10 @@ function showGrid(){
 	const ctx = canvas.getContext('2d');
 	for (let i = 0; i < canvas.width; i+=WIDTH) {
 		const cell = new Cell(i, canvas.width, i, 0);
-		cell.draw(ctx);
+		cell.draw(ctx, "white");
 		for (let j = 0; j < canvas.width; j+=WIDTH) {
 			const cell = new Cell(0, j, canvas.height, j);
-			cell.draw(ctx);
+			cell.draw(ctx, "red");
 		}
 	}
 }
@@ -90,8 +116,9 @@ class Cell {
 		this.XEnd = XEnd;
 		this.YStart = YStart;
 		this.YEnd = YEnd;
-		this.draw = function (ctx) {
+		this.draw = function (ctx, color="black") {
 			ctx.beginPath();
+			ctx.strokeStyle = color;
 			ctx.moveTo(XStart, YStart);
 			ctx.lineTo(XEnd, YEnd);
 			ctx.stroke();
